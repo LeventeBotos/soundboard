e2d1fd465db073025ae3bb50b41c27df55010bd6
Levente Botos <botos.levente2007@gmail.com>
2023-09-20 20:22:23 +0200

    first commit

--- Assets/PlaySoundd.cs
+++ Assets/PlaySoundd.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+
 public class PlaySoundd : MonoBehaviour
 {
-
     public AudioSource src;
-    // Start is called before the first frame update
+    public char keyChar = 'q'; // Change the variable name to 'keyChar' and set it to 'H' by default.
+
     void Start()
     {
-        // src.Play();
+        // src.Play(); // You can remove this line if you don't want the audio to play at the start.
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(keyChar.ToString())) // Convert the char to a string to compare with KeyCode.
         {
             src.Play();
         }
